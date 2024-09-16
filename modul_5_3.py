561620407a3c23fa56e4c828694f0e08a8c9f315
sws4sog23 <sws4sog23@gmail.com>
2024-09-17 00:36:21 +0300

    Add files via upload

--- modul_5_3.py
+++ modul_5_3.py
@@ -21,23 +21,42 @@ class House:
         return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+
 
     def __gt__(self, other):
-        return self.number_of_floors > other
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+
 
     def __ge__(self, other):
-        return self.number_of_floors >= other
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+
+
 
     def __ne__(self, other):
         return self.number_of_floors != other
 
     def __add__(self, value):
-       self.number_of_floors += value
-       return self
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        elif isinstance(value, int):
+            self.number_of_floors += value
+        return self
 
 
     def __radd__(self, value):
